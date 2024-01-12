2c1555fc56071ddb755736893674da78df279aa8
utiyama <ninjin0604@gmail.com>
2024-01-12 16:07:15 +0900

    リファクタリング

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,38 @@
 import { Hono } from "hono";
 import { stream } from "hono/streaming";
+import { renderToReadableStream } from "hono/jsx/streaming";
 
 const app = new Hono();
 
+const Html = () => {
+  return (
+    <html lang="ja">
+      <body>
+        <template shadowrootmode="open">
+          <slot name="component1">
+            <p>Loading...</p>
+          </slot>
+        </template>
+      </body>
+    </html>
+  );
+};
+
+const Component1 = async () => {
+  await sleep(2000);
+  return (
+    <div slot="component1">
+      <p>
+        <b>Hello World!!</b>
+      </p>
+    </div>
+  );
+};
+
 app.get("/", (c) => {
   return stream(c, async (stream) => {
-    await stream.write(`
-      <html lang="ja">
-        <body>
-          <template shadowrootmode="open">
-            <slot name="component1">
-              <p>Loading...</p>
-            </slot>
-          </template>
-    `);
-
-    await sleep(2000);
-
-    await stream.write(`
-          <div slot="component1">
-            <p>
-              <b>Hello World!!</b>
-            </p>
-          </div>
-        </body>
-      </html>
-    `);
+    await stream.pipe(renderToReadableStream(<Html />));
+    await stream.pipe(renderToReadableStream(<Component1 />));
   });
 });
 
